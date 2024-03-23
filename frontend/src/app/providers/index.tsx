6da2bacd9c09640ec1fa5d6642f3aca2d80997e9
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-03-23 22:33:38 +0300

    feat(frontend): register router provider in global providers

---
+++ frontend/src/app/providers/index.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import RouterProvider from "@app/providers/router-provider";
+
+const Providers: FC = () => {
+    return (
+        <>
+            <RouterProvider/>
+        </>
+    );
+};
+
+export default Providers;
