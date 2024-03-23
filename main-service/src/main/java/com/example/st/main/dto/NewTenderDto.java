0874d09809d8264a591ba9acb4d1e9165dddedc9
Lefski <93370391+Lefski@users.noreply.github.com>
2024-03-23 20:14:08 +0300

    Добавил NewTenderDto и NewTenderMapper для получения тендера

--- main-service/src/main/java/com/example/st/main/dto/NewTenderDto.java
+++ main-service/src/main/java/com/example/st/main/dto/NewTenderDto.java
@@ -2,18 +2,20 @@ package com.example.st.main.dto;
 
 import com.example.st.main.model.Currency;
 import com.example.st.main.model.Status;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
- * Класс, представляющий объект передачи данных (DTO) для отображения тендера.
+ * Класс, представляющий объект передачи данных (DTO) для сохранения тендера.
  */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TenderDto {
+public class NewTenderDto {
 
     /**
      * Формат даты и времени для отображения.
@@ -23,39 +25,48 @@ public class TenderDto {
     /**
      * Валюта тендера.
      */
+    @NotNull(message = "currency may not be null")
     private Currency currency;
 
     /**
      * Статус тендера.
      */
+    @NotNull(message = "status may not be null")
     private Status status;
 
     /**
      * Название тендера.
      */
+    @NotNull(message = "name may not be null")
+    @Size(max = 2000, min = 20, message = "name size should be between 20 and 2000 symbols")
     private String name;
 
     /**
      * Дата и время начала тендера (в формате строки).
      */
+    @NotNull(message = "startDate may not be null or have non-String format")
     @DateTimeFormat(pattern = DATE_TIME_PATTERN)
     private String startDate;
 
     /**
      * Дата и время окончания тендера (в формате строки).
      */
+    @NotNull(message = "endDate may not be null or have non-String format")
     @DateTimeFormat(pattern = DATE_TIME_PATTERN)
     private String endDate;
 
     /**
      * Дата и время публикации тендера (в формате строки).
      */
+    @NotNull(message = "publishDate may not be null or have non-String format")
     @DateTimeFormat(pattern = DATE_TIME_PATTERN)
     private String publishDate;
 
     /**
      * Название компании, проводящей тендер.
      */
+    @NotNull(message = "company name may not be null")
+    @Size(max = 2000, min = 5, message = "company name size should be between 5 and 2000 symbols")
     private String company;
 
     /**
@@ -73,3 +84,5 @@ public class TenderDto {
      */
     private String code;
 }
+
+
