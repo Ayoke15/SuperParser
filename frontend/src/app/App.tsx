92b6bccdcda67745eaee17f2073e3bef8ac52672
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-03-23 22:24:14 +0300

    feat(frontend): add header

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,10 @@
+import HeaderUI from "@shared/ui/header";
+
 function App() {
     return (
         <>
             <h1>Default React application</h1>
+            <HeaderUI/>
         </>
     )
 }
