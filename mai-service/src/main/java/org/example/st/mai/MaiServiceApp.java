ac3c479bc1aa1f698f3e3d87bb0e2f8ba134f54f
Lefski <93370391+Lefski@users.noreply.github.com>
2024-03-24 19:55:03 +0300

    Создал модуль MAI

---
+++ mai-service/src/main/java/org/example/st/mai/MaiServiceApp.java
@@ -0,0 +1,17 @@
+package org.example.st.mai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MaiServiceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MaiServiceApp.class, args);
+    }
+}
