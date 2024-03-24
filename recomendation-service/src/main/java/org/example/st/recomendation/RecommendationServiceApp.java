a92ff18b7bd1aeb4dd3c87eff8c6a5ca3ce742e9
Lefski <93370391+Lefski@users.noreply.github.com>
2024-03-24 19:40:00 +0300

    Создал модуль Recomendations

---
+++ recomendation-service/src/main/java/org/example/st/recomendation/RecommendationServiceApp.java
@@ -0,0 +1,14 @@
+package org.example.st.recomendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApp
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(RecommendationServiceApp.class, args);
+    }
+}
---
+++ recomendation-service/src/test/java/org/example/st/recomendation/RecommendationServiceAppTest.java
@@ -0,0 +1,38 @@
+package org.example.st.recomendation;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple RecommendationServiceApp.
+ */
+public class RecommendationServiceAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RecommendationServiceAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RecommendationServiceAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
