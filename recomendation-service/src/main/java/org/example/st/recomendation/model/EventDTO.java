479ab3eb74da187c3b7ca6517de4f1b901373186
Arina Pashkova <arina.pashkova@list.ru>
2024-03-25 20:21:23 +0300

    add Math Model with weight

---
+++ recomendation-service/src/main/java/org/example/st/recomendation/model/EventDTO.java
@@ -0,0 +1,22 @@
+package org.example.st.recomendation.model;
+
+public class EventDTO {
+    private String filter;
+    private Boolean success;
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+}
