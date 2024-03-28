e93a8c767e8376143b8e6af94ba51f8528c07cdc
Arina Pashkova <arina.pashkova@list.ru>
2024-03-29 02:06:11 +0300

    add ebuchi filter epta

--- recomendation-service/src/main/java/org/example/st/recomendation/controller/RecondationController.java
+++ recomendation-service/src/main/java/org/example/st/recomendation/controller/RecondationController.java
@@ -1,12 +1,16 @@
 package org.example.st.recomendation.controller;
 
 import org.example.st.recomendation.model.EventDTO;
+import org.example.st.recomendation.model.FilterDTO;
+import org.example.st.recomendation.model.ShortTenderDTO;
 import org.example.st.recomendation.service.RecomendationService;
+import org.example.st.recomendation.service.TenderRankingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +19,16 @@ import java.util.Map;
 public class RecondationController {
     @Autowired
     private RecomendationService filterService; // Сервис для обработки логики
+
+    private final TenderRankingService tenderRankingService;
+
+    @Autowired
+    public RecondationController(TenderRankingService tenderRankingService) {
+        this.tenderRankingService = tenderRankingService;
+    }
+    private FilterDTO currentFilter;
+    private final List<ShortTenderDTO> tenders = new ArrayList<>();
+    private Map<String, Object> weights;
 
     @PostMapping("/update")
     public ResponseEntity<?> updateFilters(@RequestBody List<EventDTO> events) {
@@ -34,6 +48,21 @@ public class RecondationController {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(Map.of("status", "error", "message", e.getMessage()));
         }
+    }
+
+    @PostMapping("/filters")
+    public ResponseEntity<?> submitFilters(@RequestBody FilterDTO filter) {
+        this.currentFilter = filter; // Сохраняем фильтр в переменную
+        return ResponseEntity.ok().build(); // Отправляем подтверждение об успешном сохранении
+    }
+    @PostMapping("/submit")
+    public ResponseEntity<List<Long>> submitTenders(@RequestBody List<ShortTenderDTO> tenderList) {
+        this.tenders.clear();
+        this.tenders.addAll(tenderList);
+        this.weights = filterService.calculateWeights();
+        List<Long> rankedTenderIds = tenderRankingService.rankTenders(this.tenders, this.currentFilter, this.weights);
+
+        return ResponseEntity.ok(rankedTenderIds);
     }
 
 }
