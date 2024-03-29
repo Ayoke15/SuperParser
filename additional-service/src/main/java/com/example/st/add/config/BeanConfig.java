c00797ecea2833095a3c21a1019acc5733664bf0
Maxim <zhavoronkov03@inbox.ru>
2024-03-29 12:17:01 +0300

    renamed config to bean config

--- additional-service/src/main/java/com/example/st/add/config/BeanConfig.java
+++ additional-service/src/main/java/com/example/st/add/config/BeanConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class BeanConfig {
     @Bean
     public WebDriver webDriver(){
         ChromeOptions options = new ChromeOptions();
